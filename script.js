bc493f91d2cb5937cffbe4f90b7dadeaf6dc88d7
Vivek Sunil Jadhav <vivek.jadhav.edu@gmail.com>
2023-02-16 00:44:35 +0530

    Notes using HTML CSS and JS ---> First Commit

---
+++ script.js
@@ -0,0 +1,76 @@
+const addBtn = document.querySelector("#addBtn");
+const main = document.querySelector("#main");
+
+addBtn.addEventListener(
+    "click",
+    function () {
+        addNote();
+    }
+)
+
+const saveNotes = () => {
+    const notes = document.querySelectorAll(".note textarea");
+    const data = [];
+    notes.forEach(
+        (note) => {
+            data.push(note.value);
+        }
+    )
+    if (data.length === 0) {
+        localStorage.removeItem("notes");
+    } else {
+        localStorage.setItem("notes", JSON.stringify(data));
+    }
+}
+const addNote = (text = "") => {
+    const note = document.createElement("div");
+    note.classList.add("note");
+    note.innerHTML = `
+    <div class="tool">
+    <i class="fa-solid save fa-floppy-disk"></i>
+    <i class="fa-solid trash fa-trash"></i>
+    </div>
+    <textarea>${text}</textarea>`;
+    note.querySelector(".trash").addEventListener(
+        "click",
+        function () {
+            note.remove();
+            saveNotes();
+        }
+    )
+    note.querySelector(".save").addEventListener(
+        "click",
+        function () {
+            saveNotes();
+        }
+    )
+    note.querySelector("textarea").addEventListener(
+        "focusout",
+        function () {
+            saveNotes();
+        }
+    )
+    main.appendChild(note);
+    saveNotes();
+}
+
+(
+    function () {
+        const lsNotes = JSON.parse(localStorage.getItem("notes"));
+        if (lsNotes === null) {
+            addNote();
+        } else {
+            lsNotes.forEach(
+                (lsNotes) => {
+                    addNote(lsNotes);
+                }
+            )
+        }
+        if (lsNotes.lenhth === 0) {
+            localStorage.removeItem("notes")
+        }
+        else {
+            addNote();
+        }
+    }
+)()
